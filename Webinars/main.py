8c357593eccd62e1768db256a3e9efcee0485bf8
Serhii Korobchenko <jchild2008@gmail.com>
2022-08-04 23:01:33 +0300

    040822

--- Webinars/main.py
+++ Webinars/main.py
@@ -1,72 +1,37 @@
+'''
+Завдання: Сортування файлів у папці.
+Копіювати файли із зазначеної папки та покласти в нову папку з розширенням цього файлу.
+'''
+
+import argparse  # дозволяє писати консольні програми
 from pathlib import Path
-import shutil
-import sys
-import file_parser as parser
-from normalize import normalize
+from shutil import copyfile
+
+# Вважаємо що ми будемо запускати програму:
+# python main.py -s picture -o dist
+# -s => source (це короткий запис)
+# -o output (це короткий запис)
+
+parser = argparse.ArgumentParser(
+    description='Sorting folder')  # парсер командної строки, description - просто опис.
+parser.add_argument('--source', '-s', required=True, help='Source folder')  # --source ім'я змінної, довгий запис
+parser.add_argument('--output', '-o', default='dist', help='Output folder')
+args = vars(parser.parse_args())  # parser.parse_args() - парсер, vars - повертає словник
+source = args.get('source')  # із словника отримаємо значення по ключу
+output = args.get('output')  # із словника отримаємо значення по ключу
+print(source, output)
 
 
-def handle_media(filename: Path, target_folder: Path):
-    target_folder.mkdir(exist_ok=True, parents=True)
-    filename.replace(target_folder / normalize(filename.name))
+def read_folder(path: Path) -> None:
+    for el in path.iterdir():
+        if el.is_dir():
+            read_folder(el)
+        else:
+            ext = el.suffix
+            new_path = output_folder / ext
+            new_path.mkdir(exist_ok=True, parents=True)
+            copyfile(el, new_path / el.name)
 
 
-def handle_other(filename: Path, target_folder: Path):
-    target_folder.mkdir(exist_ok=True, parents=True)
-    filename.replace(target_folder / normalize(filename.name))
-
-
-def handle_archive(filename: Path, target_folder: Path):
-    # Створюємо папку для архіву
-    target_folder.mkdir(exist_ok=True, parents=True)
-    # Створюємо папку куди розпакуємо архів
-    # Беремо суфікс у файла і удаляємо replace(filename.suffix, '')
-    folder_for_file = target_folder / normalize(filename.name.replace(filename.suffix, ''))
-
-    # Створюємо папку для архіву з іменем файлу
-    folder_for_file.mkdir(exist_ok=True, parents=True)
-    try:
-        shutil.unpack_archive(str(filename.resolve()),
-                              str(folder_for_file.resolve()))
-    except shutil.ReadError:
-        print(f'Це не архів {filename}!')
-        folder_for_file.rmdir()
-        return None
-    filename.unlink()
-
-
-def handle_folder(folder: Path):
-    try:
-        folder.rmdir()
-    except OSError:
-        print(f'Помилка видалення папки {folder}')
-
-
-def main(folder: Path):
-    parser.scan(folder)
-    for file in parser.JPEG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPEG')
-    for file in parser.JPG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPG')
-    for file in parser.PNG_IMAGES:
-        handle_media(file, folder / 'images' / 'PNG')
-    for file in parser.SVG_IMAGES:
-        handle_media(file, folder / 'images' / 'SVG')
-    for file in parser.MP3_AUDIO:
-        handle_media(file, folder / 'audio' / 'MP3')
-
-    for file in parser.MY_OTHER:
-        handle_other(file, folder / 'MY_OTHER')
-    for file in parser.ARCHIVES:
-        handle_archive(file, folder / 'archives')
-
-    # Виконуємо реверс списку для того щоб видалити всі папки
-    for folder in parser.FOLDERS[::-1]:
-        handle_folder(folder)
-
-
-if __name__ == '__main__':
-    if sys.argv[1]:
-        folder_for_scan = Path(sys.argv[1])
-        
-        print(f'Start in folder {folder_for_scan.resolve()}')
-        main(folder_for_scan.resolve())
+output_folder = Path(output)  # dist
+read_folder(Path(source))
